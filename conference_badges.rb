138dd8c83788dd5451af9cd03e6c621223553097
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-05 16:08:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ end
 
 batch_badge_creator(array)
   new_array = []
-  array = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+ 
   array.each do |name|
     new_array << "Hello, my name is #{name}"
   end
